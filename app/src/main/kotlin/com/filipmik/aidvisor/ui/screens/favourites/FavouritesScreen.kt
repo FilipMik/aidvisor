fefffa4e53873fec317af7f93c5a3f60a792e153
Filip Mik <filip.mik.12@gmail.com>
2023-10-08 20:28:14 +0200

    Add favourites screen

--- app/src/main/kotlin/com/filipmik/aidvisor/ui/screens/favourites/FavouritesScreen.kt
+++ app/src/main/kotlin/com/filipmik/aidvisor/ui/screens/favourites/FavouritesScreen.kt
@@ -1,24 +1,57 @@
 package com.filipmik.aidvisor.ui.screens.favourites
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.filipmik.aidvisor.domain.model.Recipe
+import com.filipmik.aidvisor.ui.screens.home.components.RecipeListItem
 
 @Composable
 fun FavouritesScreen(
-    navController: NavController
+    navController: NavController,
+    viewModel: FavouritesViewModel = hiltViewModel()
 ) {
 
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(40.dp)
+    with(viewModel.favouritesState) {
+        Favourites.Content(
+            actions = viewModel,
+            favRecipes = recipes)
+    }
+}
+
+object Favourites {
+
+    interface Actions {
+        fun navigateToRecipeDetail(recipe: Recipe)
+    }
+
+    @Composable
+    fun Content(
+        actions: Actions,
+        favRecipes: List<Recipe>
     ) {
-        Text(text = "FavouritesScreen")
+
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            itemsIndexed(favRecipes) {index, recipe ->
+                RecipeListItem(
+                    recipe = recipe,
+                    onItemClick = {},
+                    onSaveClick = {})
+
+                if (index < favRecipes.lastIndex) {
+                    Divider(
+                        modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                        thickness = 1.dp
+                    )
+                }
+            }
+        }
     }
 }
